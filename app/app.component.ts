2cbe1129a676a5ec1150c661a23af768a5169be0
Manmeet S. Oberoi <v-maob@microsoft.com>
2016-09-06 19:19:20 +0530

    Barebone project of Angular2 App inside Electron

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+var process: any;
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
